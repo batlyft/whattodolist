152beaa8dec5639d5996f9d39a1bd03d209c4ee8
Bat Computer <Batcomputer@Bats-MacBook-Pro.local>
2015-10-07 23:32:37 -0700

    first commit

---
+++ WhatToDo/SecondViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SecondViewController.swift
+//  WhatToDo
+//
+//  Created by Bat Computer on 10/7/15.
+//  Copyright © 2015 HardSoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+}
+
